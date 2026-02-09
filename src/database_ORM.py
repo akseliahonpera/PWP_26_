83a04968b7858823d8252d7f5ebf136c387c2856
Akseli Ahonperä <aahonper20@student.oulu.fi>
2026-02-10 00:31:17 +0200

    users, jobs insertions work, db is hardcoded currently but work, db itself need to be created with shell firsthand

--- src/database_ORM.py
+++ src/database_ORM.py
@@ -1,8 +1,9 @@
+import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
 from sqlalchemy_utils import database_exists
-
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
@@ -15,23 +16,23 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique= True, nullable=False)
+    username = db.Column(db.String(20), unique= False, nullable=False) ##set unique to false to test
     password= db.Column(db.String(32), nullable=False)
-    email = db.Column(db.String(63), unique= True, nullable=False)
+    email = db.Column(db.String(63), unique= False, nullable=False) ##set unique to false to test
     address = db.Column(db.String(63), nullable=False)
     phoneNumber = db.Column(db.String(31), nullable=False)
     description = db.Column(db.String(255), nullable=False)
-    created = db.Column(db.DateTime, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
     jobs = db.relationship("Job", back_populates = "user")###relation
 
 class Job(db.Model):
     __tablename__ = 'jobs'
     id = db.Column(db.Integer, primary_key=True)
-    userID = db.Column(db.Integer,  nullable=False)
+    userID = db.Column(db.Integer, db.ForeignKey(User.id),  nullable=False)
     jobDescription = db.Column(db.String(255), nullable=False)
     timetable = db.Column(db.String(63), nullable=True)
     location = db.Column(db.String(63),nullable=False)
-    created = db.Column(db.DateTime,nullable=False)
+    created = db.Column(db.DateTime, default=datetime.datetime.now, nullable=False)
     opening_hours = db.Column(db.String(63),nullable=False)
     category = db.Column(db.String(31),nullable=False)
     user = db.relationship("User", back_populates = "jobs")###relation
@@ -51,18 +52,18 @@ def createDatabase(): #tables actually,
 def insertUser(user_packet):
     """"Gets dictionary of user values"""
     try:
-
-        user_record = User(
-            username= user_packet["username"], 
-            password= user_packet["password"],
-            email= user_packet["email"],
-            address= user_packet["address"],
-            phoneNumber= user_packet["phoneNumber"],
-            description= user_packet["description"]
-            )
-        db.session.add(user_record)
-        db.session.commit()
-        return True
+        with app.app_context():
+            user_record = User(
+                username= user_packet["username"], 
+                password= user_packet["password"],
+                email= user_packet["email"],
+                address= user_packet["address"],
+                phoneNumber= user_packet["phoneNumber"],
+                description= user_packet["description"]
+                )
+            db.session.add(user_record)
+            db.session.commit()
+            return True
     except Exception as e:
         print("failed to insert user", e)
         return False
@@ -71,18 +72,19 @@ def insertUser(user_packet):
 def insertJob(job_packet):
     """"Gets dictionary of job values"""
     try: 
-        job_record = Job(
-        userID = job_packet["userID"],
-        jobDescription = job_packet["jobDescription"],
-        timetable = job_packet["timetable"],
-        location = job_packet["location"],
-        created = job_packet["created"],
-        opening_hours = job_packet["opening_hours"],
-        category = job_packet["category"]
-        )
-        db.session.add(job_record)
-        db.session.commit()
-        return True
+        with app.app_context():
+            job_record = Job(
+            userID = job_packet["userID"],
+            jobDescription = job_packet["jobDescription"],
+            timetable = job_packet["timetable"],
+            location = job_packet["location"],
+            created = job_packet["created"],
+            opening_hours = job_packet["opening_hours"],
+            category = job_packet["category"]
+            )
+            db.session.add(job_record)
+            db.session.commit()
+            return True
     except Exception as e:
         print("insert job failure", e)
         return False
